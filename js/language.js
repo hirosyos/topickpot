35cfc509a1003af48887a61c68c62946bc1dff65
hirosyos <mu33devel1@gmail.com>
2020-06-07 01:52:07 +0900

    ウェブフォント対応

--- js/language.js
+++ js/language.js
@@ -14,6 +14,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot is a service that provides up to 100 topics at the same time.',
         resent: 'Recently chosen topic',
         schedule: 'Schedule',
+
+        aboutLang: 'language',
+        column: 'column',
+        row: 'row',
+        deleteHistory: 'delete history',
     },
     {
         //フランス語
@@ -23,6 +28,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot est un service qui fournit jusqu\'à 100 sujets en même temps.',
         resent: 'Sujet récemment choisi',
         schedule: 'Programme',
+
+        aboutLang: 'langue',
+        column: 'colonne',
+        row: 'ligne',
+        deleteHistory: 'supprimer l\'historique',
     },
     {
         //ドイツ語
@@ -32,6 +42,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot ist ein Dienst, der bis zu 100 Themen gleichzeitig anbietet.',
         resent: 'Kürzlich gewähltes Thema',
         schedule: 'Zeitplan',
+
+        aboutLang: 'Sprache',
+        column: 'Spalte',
+        row: 'Reihe',
+        deleteHistory: 'Geschichte löschen',
     },
     {
         //スペイン語
@@ -41,6 +56,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot es un servicio que proporciona hasta 100 temas al mismo tiempo.',
         resent: 'El tema elegido recientemente',
         schedule: 'Programación',
+
+        aboutLang: 'idioma',
+        column: 'columna',
+        row: 'fila',
+        deleteHistory: 'borrar el historial',
     },
     {
         //日本語
@@ -50,6 +70,11 @@ const languageResource = [
         aboutTopicPot: 'topicpotは、最大100個のトピックを同時に提供するサービスです。',
         resent: '最近選んだトピック',
         schedule: 'スケジュール',
+
+        aboutLang: '言語',
+        column: '列',
+        row: '行',
+        deleteHistory: '履歴を削除',
     },
     {
         //ロシア語
@@ -59,6 +84,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot - это услуга, которая предоставляет до 100 тем одновременно.',
         resent: 'Недавно выбранные слова',
         schedule: 'Расписание',
+
+        aboutLang: 'язык',
+        column: 'рубрика',
+        row: 'строка',
+        deleteHistory: 'история удаления',
     },
     {
         //イタリア語
@@ -68,6 +98,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot è un servizio che fornisce fino a 100 argomenti contemporaneamente.',
         resent: 'Parole scelte di recente',
         schedule: 'Programmazione',
+
+        aboutLang: 'lingua',
+        column: 'rubrica',
+        row: 'riga',
+        deleteHistory: 'cancellare la cronologia',
     },
     {
         //中国語
@@ -77,6 +112,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot是一个可以同时提供多达100个话题的服务。',
         resent: '最近选择的词语',
         schedule: '时间表',
+
+        aboutLang: '语言',
+        column: '专栏',
+        row: '行',
+        deleteHistory: '删除历史',
     },
     {
         //ポルトガル語
@@ -86,6 +126,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot é um serviço que oferece até 100 tópicos ao mesmo tempo',
         resent: 'Tema recentemente escolhido',
         schedule: 'Horário',
+
+        aboutLang: 'língua',
+        column: 'coluna',
+        row: 'linha',
+        deleteHistory: 'apagar histórico',
     },
     {
         //ポーランド語
@@ -95,6 +140,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot to usługa, która oferuje do 100 tematów jednocześnie',
         resent: 'Ostatnio wybrany temat',
         schedule: 'Rozkład jazdy',
+
+        aboutLang: 'Język',
+        column: 'kolumna',
+        row: 'Rząd',
+        deleteHistory: 'usunięcie historii',
     },
     {
         //オランダ語
@@ -104,6 +154,11 @@ const languageResource = [
         aboutTopicPot: 'topicpot is een dienst die tot 100 thema\'s tegelijk aanbiedt.',
         resent: 'Onlangs gekozen onderwerp',
         schedule: 'Plan',
+
+        aboutLang: 'taal',
+        column: 'kolom',
+        row: 'rij',
+        deleteHistory: 'verwijderingsgeschiedenis',
     },
 
 ]
@@ -115,7 +170,13 @@ const languageResource = [
  */
 function languageChange(langType) {
     $('#htmlLang').attr('lang', languageResource[langType].language);
+
+    $('#aboutLangType').text(languageResource[langType].aboutLang);
+    $('#aboutColmn').text(languageResource[langType].column);
+    $('#aboutRow').text(languageResource[langType].row);
+
     $('#aboutTopicPot').text(languageResource[langType].aboutTopicPot);
     $('#aboutResent').text(languageResource[langType].resent);
     $('#aboutSchedule').text(languageResource[langType].schedule);
+    $('#deleteHistory').text(languageResource[langType].deleteHistory);
 }
